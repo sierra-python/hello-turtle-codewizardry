7b48bb4e24437153ab051e0d1f21ed23645a473b
Frank DiDonato <frank.didonato1@gmail.com>
2019-09-06 18:45:44 -0700

    Create hello_turtle.py

---
+++ hello_turtle.py
@@ -0,0 +1,12 @@
+# Frank DiDonato - for Assignment 1 
+
+import turtle
+
+my_turtle = turtle.Turtle()  # create title
+my_turtle.color("green")     # set color
+my_turtle.forward(100)       # lift a green line of length 100
+my_turtle.up()               # lift up the tail
+my_turtle.forward(100)       # move forward 100 without drawing
+my_turtle.right(90)          # change direction to the right 90 degrees
+my_turtle.down()             # put the tails down
+my_turtle.backward(100)      # draw a green line 100 units long
